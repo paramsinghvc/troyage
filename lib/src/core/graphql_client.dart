c083f56909e6572bcb8ed7f0111e325dca7b51b0
Param <paramsinghvc@gmail.com>
2023-04-20 01:32:59 +0100

    feat: splash screen added

--- lib/src/core/graphql_client.dart
+++ lib/src/core/graphql_client.dart
@@ -1,7 +1,7 @@
 import 'package:graphql/client.dart';
 
 class TroyageGQLClient extends GraphQLClient {
-  static const TRAINLY_API_LINK = 'http://192.168.0.25:3001/graphql';
+  static const TRAINLY_API_LINK = 'https://trainly-7jtsp7t3l-paramsinghvc.vercel.app/graphql';
 
   TroyageGQLClient() : super(link: HttpLink(TRAINLY_API_LINK), cache: GraphQLCache());
 }
